1a41eb0716a202b3c6e5c81caab253846f237743
Joaquim <joaquinciudad@iesebre.com>
2023-11-06 19:34:50 +0100

    fitxers configurar bd

---
+++ app/framework/Database/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Framework\Database;
+
+use PDO;
+
+class Database
+{
+    protected $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+
+    public function selectAll($table)
+        {
+            $statement = $this->pdo->prepare("SELECT * FROM $table;");
+
+            $statement->execute();
+
+            return $statement->fetchAll(PDO::FETCH_CLASS);
+        }
+}
+
